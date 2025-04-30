e955ac6950fff47e8462403c075782910b77dc6f
Jagdevsinh <jagdevsinh1247@gmail.com>
2025-04-30 09:07:14 +0530

    Add files via upload

---
+++ prac1.py
@@ -0,0 +1,77 @@
+# a='aaa'
+# b='atmiya university'
+# print('name',a,'university',b)
+
+# name="aaa"
+# address="rajkot"
+# print("name:\n",name)
+# print("address:\n",address)
+
+# a=10
+# b=20
+# print(a+b)
+# a=10
+# b=20
+# print(a-b)
+# a=10
+# b=20
+# print(a*b)
+# a=10
+# b=20
+# print(a/b)
+# a=10
+# b=20
+# print(a%b)
+
+# p=float(input('enter amount'))
+# r=float(input('enter rate of interest'))
+# t=float(input('entre time line of amount'))
+# interest=(p*r*t)/100
+# print("simple interest",interest)
+
+# a=float(input('enter amount'))
+# b=float(input('timeline'))
+# c=float(input('interest'))
+# interest=(a*b*c)/100;
+# print('simple intrest',interest)
+
+# 5) Write a program to calculate 10% bonus of salary.
+# s= float(input('enter salary'))
+
+# bonus=s*0.10
+# print('your bonus is:',bonus)
+
+# salary= float(input('enter your salary'))
+# bonus=salary*0.10
+# print('your bonus is',bonus)
+
+# first=float(input('enter your salary'))
+# bonus=first*0.10
+# print('your bonus is',bonus)
+
+
+# last=float(input('enter pagar'))
+# bonus=last*0.50
+# print('your salary',bonus)
+
+# s=float(input('enter your varshik pagar'))
+# advance=s*.10
+# print('your varshik pagar is',advance)
+
+
+# km=float(input('enter distance'))
+# meter=km*1000
+# print('meter is ',meter)
+
+km=float(input('enter distance you want to cover'))
+meter=km*1000
+centimeter=km*100000
+feet=km*3280.84
+inchese=km*39370.1
+
+print('distance is deferent units\n')
+print(meter,"metere")
+print(centimeter,"centimeter")
+print(feet,"feet")
+print(inchese,"inchese")
+
